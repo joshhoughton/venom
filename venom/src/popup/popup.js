2533aa75ec96b63dc3160a781bcc613679271bda
Josh Houghton <shubbler@me.com>
2021-04-05 20:43:32 +0100

    Added My Bookmarks improvements

--- venom/src/popup/popup.js
+++ venom/src/popup/popup.js
@@ -1,6 +1,35 @@
-var table = document.getElementById("bookmarkTable")
-
 document.addEventListener("DOMContentLoaded", function(){  
+    document.getElementById("exportButton").addEventListener("click", (e) => {
+        chrome.storage.local.get(['timestamps'], function(result) {
+            timestamps = result.timestamps
+    
+    
+            // Sort timestamps by creation timestamp
+            timestamps.sort((a,b) => (a[2] > b[2]) ? 1 : ((b[2] > a[2]) ? -1 : 0))
+    
+            for (let index = 0; index < timestamps.length; index++) {
+                const element = timestamps[index];
+                // Convert timestamp to date string
+                timestamps[index][0] = element[0].replaceAll('#', '')
+
+                timestamps[index][2] = new Date(element[2]).toLocaleString().replace(/,/g, '-')
+                timestamps[index][3] = encodeURIComponent(timestamps[index][3])            
+            }
+    
+    
+            // Create csv from timestamps array
+            timestamps = timestamps.map(v => v.join(',')).join('\n')
+    
+            chrome.downloads.download({    
+                url: "data:text/csv;charset=utf-8,Title,Timestamp,Date,Link\n" + timestamps,
+                filename: "VenomBookmarks.csv",
+            });
+    
+            
+        });
+    });
+
+
     var table = document.getElementById("bookmarkTable")
 
     timestamps = chrome.storage.local.get(['timestamps'], (result) => {
@@ -12,7 +41,7 @@ document.addEventListener("DOMContentLoaded", function(){
         for (let index = 0; index < timestamps.length; index++) {
             const element = timestamps[index];
             // Convert timestamp to date string
-            timestamps[index][2] = new Date(element[2]).toLocaleString()
+            //timestamps[index][2] = moment(element[2]).fromNow() //new Date(element[2]).toLocaleString()
     
     
             let row = table.insertRow();
@@ -28,9 +57,16 @@ document.addEventListener("DOMContentLoaded", function(){
             cell.appendChild(text);
 
             cell = row.insertCell();
-            text = document.createTextNode(element[2]);
+            text = document.createTextNode(moment(element[2]).fromNow());
             cell.appendChild(text);
 
+            cell.addEventListener('mouseover', (e) => {
+                e.target.innerText = new Date(element[2]).toLocaleString()
+            })
+            
+            cell.addEventListener('mouseout', (e) => {
+                e.target.innerText = moment(element[2]).fromNow()
+            })
 
         }
     })
